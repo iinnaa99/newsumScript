0f2654722e577e0bc5d1e860b0cd05ec4d87b91c
jiho99-hub <jehoa99@gmail.com>
2025-07-16 07:30:02 +0000

    shellScript commit

---
+++ shell/delete-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "CronJob 삭제"
+kubectl delete -f cronjob.yaml --ignore-not-found
+
+echo "CronJob용 Secret 삭제"
+kubectl delete -f secret.yaml --ignore-not-found
+
+echo "웹앱 리소스 삭제 (HPA, Service, Deployment, ConfigMap)"
+kubectl delete -f newsum-service-hpa.yaml --ignore-not-found
+kubectl delete -f newsum-deployment.yaml --ignore-not-found
+kubectl delete -f newsum-configmap.yaml --ignore-not-found
+
+echo "DB 리소스 삭제 (Service, StatefulSet, ConfigMap)"
+kubectl delete -f mysql-service.yaml --ignore-not-found
+kubectl delete -f mysql-statefulset.yaml --ignore-not-found
+kubectl delete -f db-configmap.yaml --ignore-not-found
+
+echo "pvc, pv 삭제"
+kubectl delete pvc data-mysql-db-0 -n site
+kubectl delete pv raid-pv -n site
+
+echo "----- 모든 리소스가 성공적으로 삭제되었습니다 -----"
